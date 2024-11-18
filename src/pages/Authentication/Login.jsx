9fe26bf52cf4994250106b27a00bb7537641b040
codewithsaidul <codewithsaidul@gmail.com>
2024-11-18 21:17:52 +0600

    login page email & password field auto submit problem solve

--- src/pages/Authentication/Login.jsx
+++ src/pages/Authentication/Login.jsx
@@ -48,7 +48,7 @@ const Login = () => {
           </p>
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form onClick={handleSubmit(onSubmit)} className="card-body">
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
